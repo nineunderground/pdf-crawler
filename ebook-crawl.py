3624a6020c5a6b3f8194f354d5be4dd98c63cba4
Iñaki Rodriguez <nineunderground@gmail.com>
2018-08-02 14:05:39 +0300

    Initial commit
    Just testing

---
+++ ebook-crawl.py
@@ -0,0 +1,6 @@
+#!/bin/bash
+# It does a web url called to attempt a login request tot he ebook service.
+# Then it uses the given token for the claim pdf
+
+echo "Calling to the PDF service..."
+sleep 1.5
